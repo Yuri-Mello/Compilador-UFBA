11285bbaf1d050da621c40d03cb0d4d5acc02a33
Yuri-Mello <55611468+Yuri-Mello@users.noreply.github.com>
2019-09-21 13:59:13 -0300

    Update lexico.cpp

--- lexico.cpp
+++ lexico.cpp
@@ -1,17 +1,23 @@
 /*
 PALAVRAS RESERVADAS:
-PROGRAMAINICIO, EXECUCAOINICIO, FIMEXECUCAO, FIMPROGRAMA, DEFINAINSTRUCAO, COMO, INICIO, FIM,
-REPITA, VEZES, FIMREPITA, ENQUANTO, FACA, FIMPARA, SE, ENTAO, FIMSE, SENAO, FIMSENAO, MOVA, PASSOS,
-VIRE PARA, PARE, FINALIZE, APAGUE LAMPADA, ACENDA LAMPADA, AGUARDE ATE, ROBO PRONTO, ROBO OCUPADO,
-ROBO PARADO, ROBO MOVIMENTADO, FRENTE ROBO BLOQUEADA, DIREITA ROBO BLOQUEADA, ESQUERDA ROBO BLOQUEADA,
-LAMPADA ACESA A FRENTE, LAMPADA APAGADA A FRENTE, LAMPADA ACESSA A ESQUERDA, LAMPADA APAGADA A ESQUERDA,
-LAMPADA ACESA A DIREITA, LAMPADA APAGADA A DIREITA, ESQUERDA, DIREITA.
+ACENDA LAMPADA, APAGUE LAMPADA, AGUARDE ATE,
+COMO,
+DEFINAINSTRUCAO, DIREITA, DIREITA ROBO BLOQUEADA,
+ENQUANTO, ENTAO, ESQUERDA, ESQUERDA ROBO BLOQUEADA, EXECUCAOINICIO,
+FACA, FIM, FIMEXECUCAO, FIMPARA, FIMPROGRAMA, FIMREPITA, FIMSE, FIMSENAO, FINALIZE, FRENTE ROBO BLOQUEADA,
+INICIO,
+LAMPADA ACESA A DIREITA, LAMPADA ACESA A ESQUERDA, LAMPADA ACESA A FRENTE,
+LAMPADA APAGADA A DIREITA, LAMPADA APAGADA A ESQUERDA, LAMPADA APAGADA A FRENTE, 
+MOVA,
+PARE, PASSOS, PROGRAMAINICIO,
+REPITA, ROBO MOVIMENTADO, ROBO OCUPADO, ROBO PARADO, ROBO PRONTO,
+SE, SENAO,
+VEZES, VIRE PARA.
 */
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -28,12 +34,13 @@ token novoToken(string tipo, string texto) {
 }
 
 void erroLexico(int linha, int coluna) {
-	cout << "Erro: erro léxico na linha " << linha << ", coluna " << coluna << ". \n";
+	cout << "Erro: erro léxico na linha " << linha << ", coluna " << coluna << ".\n";
 }
 
-vector<token> analisadorLexico(char buffer[]) {
+vector<token> analisadorLexico(char buffer[], map<string,string> reservadas) {
 	vector<token> tokens;
-	int linha = 1, coluna = 1, estado = 0, i = 0, aux = 0;
+	int linha = 1, coluna = 1, estado = 0, i = 0;
+	string texto = "";
 	while(true) {
 		switch(estado) {
 			// caso de erro
@@ -52,7 +59,7 @@ vector<token> analisadorLexico(char buffer[]) {
 					i++;
 				}
 				else if (buffer[i] >= 48 && buffer[i] <= 57) {
-					aux = 0;
+					texto = "";
 					estado = 1;
 				}
 				else if ((buffer[i] >= 65 && buffer[i] <= 90) || 
@@ -66,15 +73,11 @@ vector<token> analisadorLexico(char buffer[]) {
 			// caso NUMERO
 			case 1:
 				if (buffer[i] >= 48 && buffer[i] <= 57) {
-					aux++;
+					texto += buffer[i];
 					coluna++;
 					i++;
 				}
 				else if (buffer[i] == '\t' || buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\0') {
-					string texto = "";
-					for (int j = 0; j < aux; j++) {
-						texto += buffer[i + j - aux];
-					}
 					tokens.push_back(novoToken("numero",texto));
 
 					estado = 0;
@@ -95,9 +98,23 @@ vector<token> analisadorLexico(char buffer[]) {
 
 
 int main() {
-	char buffer[3] = {'1','2','3'};
-	vector<token> teste = analisadorLexico(buffer);
+	char buffer[8] = {'1','2','3',' ','3','\n','2','3'};
 
-	cout << teste[0].texto << " " << teste[0].tipo << endl;
+	// mapa das palavras reservadas
+	// (incompleto pq sou preguiçoso)
+	map<string,string> reservadas;
+	reservadas["ACENDA LAMPADA"] = "ACENDA LAPADA"; reservadas["APAGUE LAMPADA"] = "APAGUE LAMPADA";
+	reservadas["AGUARDE ATE"] = "AGUARDE ATE"; reservadas["COMO"] = "COMO";
+	reservadas["DEFINAINSTRUCAO"] = "DEFINAINSTRUCAO"; reservadas["DIREITA"] = "DIREITA";
+	reservadas["DIREITA ROBO BLOQUEADA"] = "DIREITA ROBO BLOQUEADA"; 
+
+	vector<token> teste = analisadorLexico(buffer,reservadas);
+
+	if (!teste.empty())
+		for (int i = 0; i < teste.size(); i++)
+		cout << teste[i].texto << " " << teste[i].tipo << endl;
+	else
+		cout << "empty" << endl;
+
     return 0;
 }
