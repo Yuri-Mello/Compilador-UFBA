366cdba7220d01eb3d388ab717cbe41616c3b3a4
Yuri-Mello <55611468+Yuri-Mello@users.noreply.github.com>
2019-11-04 21:00:51 -0300

    Update lexico.h

--- parte2/lexico.h
+++ parte2/lexico.h
@@ -15,12 +15,9 @@ SE, SENAO,
 VEZES, VIRE PARA.
 */
 #include <iostream>
-#include <fstream>
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include <map>
-#include <cctype>
 
 using namespace std;
 
@@ -45,11 +42,60 @@ void erroLexico(int linha, int coluna) {
 	cout << "ERRO: erro léxico na linha " << linha << ", coluna " << coluna << ".\n";
 }
 
-vector<token> analisadorLexico(char buffer[], map<string,string> reservadas) {
+vector<token> analisadorLexico(char buffer[]) {
+	// mapa das palavras reservadas
+	map<string,string> reservadas;
+		reservadas["ACENDA"]="ACENDA";
+		reservadas["LAMPADA"]="LAMPADA";
+		reservadas["APAGUE"]="APAGUE";
+		reservadas["LAMPADA"]="LAMPADA";
+		reservadas["AGUARDE"]="AGUARDE";
+		reservadas["ATE"]="ATE";
+		reservadas["COMO"]="COMO";
+		reservadas["DEFINAINSTRUCAO"]="DEFINAINSTRUCAO";
+		reservadas["DIREITA"]="DIREITA";
+		reservadas["ROBO"]="ROBO";
+		reservadas["BLOQUEADA"]="BLOQUEADA";
+		reservadas["ENQUANTO"]="ENQUANTO";
+		reservadas["ENTAO"]="ENTAO";
+		reservadas["ESQUERDA"]="ESQUERDA";
+		reservadas["EXECUCAOINICIO"]="EXECUCAOINICIO";
+		reservadas["FACA"]="FACA";
+		reservadas["FIM"]="FIM";
+		reservadas["FIMEXECUCAO"]="FIMEXECUCAO";
+		reservadas["FIMPARA"]="FIMPARA";
+		reservadas["FIMPROGRAMA"]="FIMPROGRAMA";
+		reservadas["FIMREPITA"]="FIMREPITA";
+		reservadas["FIMSE"]="FIMSE";
+		reservadas["FIMSENAO"]="FIMSENAO";
+		reservadas["FINALIZE"]="FINALIZE";
+		reservadas["FRENTE"]="FRENTE";
+		reservadas["INICIO"]="INICIO";
+		reservadas["ACESA"]="ACESA";
+		reservadas["A"]="A";
+		reservadas["APAGADA"]="APAGADA";
+		reservadas["MOVA"]="MOVA";
+		reservadas["PARE"]="PARE";
+		reservadas["PASSO"]="PASSO";
+		reservadas["PASSOS"]="PASSOS";
+		reservadas["PROGRAMAINICIO"]="PROGRAMAINICIO";
+		reservadas["REPITA"]="REPITA";
+		reservadas["MOVIMENTANDO"]="MOVIMENTANDO";
+		reservadas["OCUPADO"]="OCUPADO";
+		reservadas["PARADO"]="PARADO";
+		reservadas["PRONTO"]="PRONTO";
+		reservadas["SE"]="SE";
+		reservadas["SENAO"]="SENAO";
+		reservadas["VEZ"]="VEZ";
+		reservadas["VEZES"]="VEZES";
+		reservadas["VIRE"]="VIRE";
+		reservadas["PARA"]="PARA";
+
 	bool newLine = true; // checa se a linha é nova para comentários
 	vector<token> tokens;
 	int linha = 1, coluna = 1, estado = 0, i = 0;
 	string texto = "";
+
 	while(true) {
 		switch(estado) {
 			// caso de erro
